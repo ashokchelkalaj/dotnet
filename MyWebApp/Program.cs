69d5d7a3e49482d8bc58e59441a11e0078a9881c
ashokchelkalaj <114649095+ashokchelkalaj@users.noreply.github.com>
2022-10-27 14:35:27 +0530

    Update Program.cs

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+app.Urls.Add("https://20.197.1.17:5000"); //linux IP
+app.Urls.Add("https://127.0.0.1:5000"); //127.0.0.1 IP
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
